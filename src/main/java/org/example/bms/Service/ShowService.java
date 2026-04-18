0c48fc4041017b021f0e1540c1ffb7211b8c82ae
Rajdeepsinh Pravinbhai Rathod <rajdeeprathod453@gmail.com>
2026-04-18 22:22:54 +0530

    created creat show method

--- src/main/java/org/example/bms/Service/ShowService.java
+++ src/main/java/org/example/bms/Service/ShowService.java
@@ -1,12 +1,15 @@
 package org.example.bms.Service;
 
 
-import org.example.bms.dto.ShowDto;
+import org.example.bms.dto.*;
 import org.example.bms.model.Movie;
 import org.example.bms.model.Screen;
 import org.example.bms.model.Show;
+import org.example.bms.model.ShowSeat;
 import org.example.bms.reposatory.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class ShowService {
     @Autowired
@@ -28,9 +31,62 @@ public class ShowService {
         show.setMovie(movie);
         Screen screen=screenRepository.getById(showDto.getScreen().getId());
         show.setScreen(screen);
+        Show savedShow=showRepository.save(show);
 
+        List<ShowSeat> availableSeats=
+                showSeatRepository.findByShowIdAndStatus(savedShow.getId(),"AVAILABLE");
+return mapToDto(savedShow,availableSeats);
     }
-public ShowDto mapToDto(Show show){
+public ShowDto mapToDto(Show show,List<ShowSeat> availableSeats){
+    ShowDto showDto=new ShowDto();
+    showDto.setId(show.getId());
+    showDto.setStartTime(show.getStartTime());
+    showDto.setEndTime(show.getEndTime());
+    showDto.setMovie(
+            new MovieDto(
+                    show.getMovie().getId(),
+                    show.getMovie().getTitle(),
+                    show.getMovie().getDescription(),
+                    show.getMovie().getLanguage(),
+                    show.getMovie().getGenre(),
+                    show.getMovie().getDurationMins(),
+                    show.getMovie().getReleaseDate(),
+                    show.getMovie().getPosterUrl()
+            )
+    );
+    //creating theatre dto for  screendto
+    TheaterDto  theaterDto=new TheaterDto();
+    theaterDto.setId(show.getScreen().getTheater().getId());
+    theaterDto.setName(show.getScreen().getTheater().getName());
+    theaterDto.setAddress(show.getScreen().getTheater().getAddress());
+    theaterDto.setCity(show.getScreen().getTheater().getCity());
+    theaterDto.setTotalScreens(show.getScreen().getTheater().getTotalScreens());
+    showDto.setScreen(
+            new ScreenDto(
+                    show.getScreen().getId(),
+                    show.getScreen().getName(),
+                    show.getScreen().getTotalSeats(),
+                    theaterDto
+            )
+    );
+List<ShowSeatDto>seatDtos=availableSeats.stream().map(
+        seat->{
+            ShowSeatDto showSeatDto=new ShowSeatDto();
+            showSeatDto.setId(seat.getId());
+            showSeatDto.setStatus(seat.getStatus());
+            showSeatDto.setPrice(seat.getPrice());
+            SeatDto seatDto=new SeatDto();
+            seatDto.setId(seat.getSeat().getId());
+            seatDto.setSeatNumber(seat.getSeat().getSeatNumber());
+            seatDto.setSeatType(seat.getSeat().getSeatType());
+            showSeatDto.setSeat(seatDto);
+            return showSeatDto;
+        }
+).toList();
+showDto.setAvailableSeats(seatDtos);
+
+
+    return showDto;
 
 }
 }
