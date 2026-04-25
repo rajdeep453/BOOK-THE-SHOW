1df7fb6aa91c26578d32e6b751eb98521f5cafa5
Rajdeepsinh Pravinbhai Rathod <rajdeeprathod453@gmail.com>
2026-04-25 21:20:17 +0530

    code updated

--- src/main/java/org/example/bms/Service/UserService.java
+++ src/main/java/org/example/bms/Service/UserService.java
@@ -1,6 +1,7 @@
 package org.example.bms.Service;
 
 import org.example.bms.dto.UserDto;
+import org.example.bms.model.User;
 import org.example.bms.reposatory.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,7 +11,13 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
     public UserDto createUser(UserDto userDto){
-
+User useer=mapToEntity(userDto);
     }
 
+    public User mapToEntity(UserDto userDto){
+        User user=new User();
+        user.setName(userDto.getName());
+        user.setPhoneNumber(userDto.getPhoneNumber());
+        return  user;
+    }
 }
